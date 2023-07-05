40859799ed7794ed757de0161ef8be43389e4ad8
Madbeyk <madbeyk@gmail.com>
2023-07-05 13:50:33 +0200

    Q1

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'mfnet.cz',
+    description: 'Marek Förster',
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
